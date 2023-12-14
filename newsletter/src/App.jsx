1b84e444cefdb6b224ffd0e3f0f308409539a050
Miguel Santos <miguel.santos6700@gmail.com>
2023-12-14 15:41:52 -0300

    Added main structure in App.jsx

--- newsletter/src/App.jsx
+++ newsletter/src/App.jsx
@@ -1,7 +1,41 @@
-// import { PlanePaperRight } from 'phosphor-icons'
+import { EnvelopeSimple } from 'phosphor-react'
+import newsletter from '../src/assets/newsletter.png'
+
+import axios from 'axios'
 
 export default function App() {
+
+  function handleForm(event) {
+    event.preventDefault()
+
+    return console.log("Nothing here yet")
+  }
+
   return(
-    <h1> Home </h1>
+    <>
+
+      <header>
+      
+        <img src={newsletter} />
+
+      </header>    
+
+      <main>
+
+        <p> 
+          Se inscreva em nosso newsletter e fique sempre atualizado! 
+        </p>
+
+        <form onSubmit={ handleForm } action="post">
+          <span>
+            <input type="email" placeholder='DIGITE SEU EMAIL'/>
+            <EnvelopeSimple size={96} color="#00ff6e" />
+          </span>
+            <input type='submit' value="ENVIAR"/>
+        </form>
+
+      </main>
+
+    </>
   )
 }
